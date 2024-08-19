9277a027d21e910d8cc55430eb01f40e4e2ff4f2
zgh836 <a3108664127@outlook.com>
2024-08-19 21:31:54 +0800

    ep009

---
+++ Ep009/src/stats/stats.cpp
@@ -0,0 +1,8 @@
+#include "supermath.h"
+double mean(int* values, int count) {
+    int sum{0};
+    for (int i=0;i<count; i++) {
+        sum = add(sum, values[i]);
+    }
+    return (double)sum /count;
+}
